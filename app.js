28c0670309fcd78820bb853c2487aa1b2cf6e601
Gian Marlo M. Tullao <yaymarlo@gmail.com>
2021-11-02 09:51:06 +0800

    WebView implementation

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const { app, BrowserWindow } = require('electron');
 
 function createWindow () {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 900,
+        height: 600,
     })
 
-    win.loadUrl('https://blueprogrammer212.github.io');
+    win.loadURL('https://blueprogrammer212.github.io/home');
 };
 
 app.whenReady().then(() => {
